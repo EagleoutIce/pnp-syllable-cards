da94c3d070bbf9d73c679e4acc41108bc02d2560
EagleoutIce <maeusepiep99@web.de>
2021-03-13 23:16:18 +0100

    imagemagick policies :)

--- create-sheets.sh
+++ create-sheets.sh
@@ -5,16 +5,16 @@ PDFFILE="card-builder.pdf"
 # Get Last Page
 lastpage=$(($(pdfinfo ${PDFFILE} | awk '/^Pages:/ {print $2}')-1))
 # -colorspace RGB
-sudo convert -dither FloydSteinberg -density 500 "${PDFFILE}" temp%d.png > /dev/null
+convert -dither FloydSteinberg -density 500 "${PDFFILE}" temp%d.png > /dev/null
 
 STEPSIZE=70
 START=0
 for (( I=$STEPSIZE; I<=$lastpage+$STEPSIZE-1; I+=$STEPSIZE )) do
     END=$(($I>$lastpage ? $lastpage : $I-1))
     echo $START--$END
-    eval sudo montage -tile 10x7 -geometry 400x500+0+0 temp{$START..$END}.png cards-${START}-$END.png
+    eval montage -tile 10x7 -geometry 400x500+0+0 temp{$START..$END}.png cards-${START}-$END.png
     START=$I
 done
 
 echo Cleanup
-sudo rm temp*.png
+rm temp*.png
