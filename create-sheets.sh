ab45f4a9548048a39ecd53d93668e28bcb634f38
EagleoutIce <maeusepiep99@web.de>
2021-03-13 22:35:38 +0100

    trying with sudo :)

--- create-sheets.sh
+++ create-sheets.sh
@@ -5,16 +5,16 @@ PDFFILE="card-builder.pdf"
 # Get Last Page
 lastpage=$(($(pdfinfo ${PDFFILE} | awk '/^Pages:/ {print $2}')-1))
 # -colorspace RGB
-convert -dither FloydSteinberg -density 500 "${PDFFILE}" temp%d.png > /dev/null
+sudo convert -dither FloydSteinberg -density 500 "${PDFFILE}" temp%d.png > /dev/null
 
 STEPSIZE=70
 START=0
 for (( I=$STEPSIZE; I<=$lastpage+$STEPSIZE-1; I+=$STEPSIZE )) do
     END=$(($I>$lastpage ? $lastpage : $I-1))
     echo $START--$END
-    eval montage -tile 10x7 -geometry 400x500+0+0 temp{$START..$END}.png cards-${START}-$END.png
+    eval sudo montage -tile 10x7 -geometry 400x500+0+0 temp{$START..$END}.png cards-${START}-$END.png
     START=$I
 done
 
 echo Cleanup
-rm temp*.png
+sudo rm temp*.png
